e4760d2e2b8bc74ef124172f78cc6cd259ad828b
u00938 <yo01030184262@gmail.com>
2021-02-27 18:53:34 +0900

    completed issue#6

--- src/controller/logout.ts
+++ src/controller/logout.ts
@@ -1,8 +1,29 @@
-import { Request, Response } from "express"
-import * as sequelize from "sequelize"
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 export const controller = {
   post: async (req: Request, res: Response) => {
-
+    try {
+      jwt.verify(
+        req.cookies.accessToken, 
+        process.env.ACCESS_SECRET!,
+        (err: any, decoded: any) => {
+          if (err) res.status(400).json({ data: null, message: err.message })
+          if (decoded) {
+            res.clearCookie("accessToken", {
+              //domain: "localhost",
+              path: "/",
+              httpOnly: true,
+              secure: true,
+              sameSite: "none",
+              maxAge: 1000 * 60 * 60 * 24,
+              overwrite: true,
+            })
+            res.status(200).json({ data: null, message: "ok" })
+          }
+      }) 
+    } catch (err) {
+      console.log(err.message)
+    }
   }
 }
