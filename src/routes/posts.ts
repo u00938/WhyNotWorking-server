2636670967684536492d7ac5239b396f832c75e6
u00938 <yo01030184262@gmail.com>
2021-03-05 23:13:57 +0900

    add token middleware

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import { controller } from "../controller/posts"
+import { tokenChecker } from "../middleware/token"
 
 export const posts = Router()
 
-posts.get('/', controller.get)
-posts.post('/', controller.post)
-posts.patch('/', controller.patch)
-posts.delete('/', controller.delete)
-posts.patch('/viewsUp', controller.viewsUp)
-posts.patch('/votesUp', controller.votesUp)
-posts.patch('/votesDown', controller.votesDown)
+posts.get('/', tokenChecker, controller.get)
+posts.post('/', tokenChecker, controller.post)
+posts.patch('/', tokenChecker, controller.patch)
+posts.delete('/', tokenChecker, controller.delete)
+posts.patch('/viewsUp', tokenChecker, controller.viewsUp)
+posts.patch('/votesUp', tokenChecker, controller.votesUp)
+posts.patch('/votesDown', tokenChecker, controller.votesDown)
