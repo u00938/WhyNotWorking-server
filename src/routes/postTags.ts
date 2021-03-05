cc7275db71ad56ad0905c6d7527f7e8caf190fb3
u00938 <yo01030184262@gmail.com>
2021-03-05 23:13:41 +0900

    add token middleware

--- src/routes/postTags.ts
+++ src/routes/postTags.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import { controller } from "../controller/postTags"
+import { tokenChecker } from "../middleware/token"
 
 export const postTags = Router();
 
-postTags.post('/', controller.post);
-postTags.delete('/', controller.delete);
+postTags.post('/', tokenChecker, controller.post);
+postTags.delete('/', tokenChecker, controller.delete);
