8ed3737729b8da85bd50544880b61dd7c7142e11
u00938 <yo01030184262@gmail.com>
2021-03-07 16:58:22 +0900

    feature_issue#26

---
+++ dist/middleware/token.js
@@ -0,0 +1,56 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.tokenChecker = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const generateToken = (payload) => {
+    return new Promise((resolve, reject) => {
+        jsonwebtoken_1.default.sign(payload, process.env.ACCESS_SECRET, { expiresIn: "1d" }, (error, token) => {
+            if (error)
+                reject(error);
+            else
+                resolve(token);
+        });
+    });
+};
+const tokenChecker = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const token = req.cookies.accessToken;
+        if (token) {
+            const payload = yield jsonwebtoken_1.default.verify(token, process.env.ACCESS_SECRET);
+            delete payload.iat;
+            delete payload.exp;
+            const newToken = yield generateToken(payload);
+            const options = {
+                //domain: "localhost",
+                path: "/",
+                httpOnly: true,
+                //secure: true,
+                //sameSite: "none",
+                maxAge: 1000 * 60 * 60 * 24,
+                overwrite: true,
+            };
+            res.cookie("accessToken", newToken, options);
+            next();
+        }
+        else {
+            res.status(400).json({ message: "auth error" });
+        }
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+});
+exports.tokenChecker = tokenChecker;
+//# sourceMappingURL=token.js.map
