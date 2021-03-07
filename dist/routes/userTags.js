55519bfab00e81b30eb0fb31ce85b276f5734a00
power100413 <power100413@naver.com>
2021-03-07 20:24:29 +0900

    userTags_add_tokenChecker

--- dist/routes/userTags.js
+++ dist/routes/userTags.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.userTags = void 0;
 const express_1 = require("express");
 const userTags_1 = require("../controller/userTags");
+const token_1 = require("../middleware/token");
 exports.userTags = express_1.Router();
-exports.userTags.post('/', userTags_1.controller.post);
-exports.userTags.delete('/', userTags_1.controller.delete);
+exports.userTags.post('/', token_1.tokenChecker, userTags_1.controller.post);
+exports.userTags.delete('/', token_1.tokenChecker, userTags_1.controller.delete);
 //# sourceMappingURL=userTags.js.map
--- src/routes/userTags.ts
+++ src/routes/userTags.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import { controller } from "../controller/userTags"
+import { tokenChecker } from "../middleware/token"
 
 export const userTags = Router();
 
-userTags.post('/', controller.post);
-userTags.delete('/', controller.delete);
+userTags.post('/', tokenChecker, controller.post);
+userTags.delete('/', tokenChecker, controller.delete);
