8af7bc15402034d218b31a9e5dfb5e9babea9039
power100413 <power100413@naver.com>
2021-03-12 16:11:48 +0900

    fix_patch

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { controller } from "../controller/users"
 import { tokenChecker } from "../middleware/token"
+import multer from "multer";
+const upload = multer();
 
 export const users = Router()
 
 users.get('/', tokenChecker, controller.get)
 users.get('/count', tokenChecker, controller.getCount)
 users.post('/', controller.signUp)
-users.patch('/', tokenChecker, controller.patch)
+users.patch('/', [tokenChecker,upload.single("image")], controller.patch)
